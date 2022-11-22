9b9f2b710402c4bc6104dd978032eaf7ae832e58
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2022-11-22 10:38:13 +0600

    Update backend.js

--- src/pages/api/backend.js
+++ src/pages/api/backend.js
@@ -1,4 +1,4 @@
-const backend = "http://localhost:3200/";
-// const backend = "https://connecteen-node-002.herokuapp.com/";
+//const backend = "http://localhost:3200/";
+const backend = "https://connecteen-node-002.herokuapp.com/";
 
 export default backend;
