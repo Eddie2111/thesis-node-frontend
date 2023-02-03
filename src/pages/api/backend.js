a6023e76736a5c5d1e374c95080db59bb2e5adf1
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2023-02-04 02:42:46 +0600

    Update backend.js

--- src/pages/api/backend.js
+++ src/pages/api/backend.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-//const backend = "http://localhost:3200/";
-const backend = "https://bdslp.onrender.com/";
+// const backend = "http://localhost:3200/";
+// const backend = "https://bdslp.onrender.com/";
+const backend = "https://calm-gold-moccasins.cyclic.app/";
 
 export const Run = async()=>{
     await axios.get(backend,{withCredentials: true})
