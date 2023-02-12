9457c143b4c9ac88e5cde1eb8db3ef0e62579a75
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2023-02-12 21:08:38 +0600

    Update backend.js

--- src/pages/api/backend.js
+++ src/pages/api/backend.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const backend = "http://localhost:3200/";
-// const backend = "https://bdslp.onrender.com/";
-const backend = "https://easy-pink-basket-clam-cuff.cyclic.app/";
+const backend = "https://bdslp.onrender.com/";
+// const backend = "https://easy-pink-basket-clam-cuff.cyclic.app/";
 
 export const Run = async()=>{
     await axios.get(backend,{withCredentials: true})
